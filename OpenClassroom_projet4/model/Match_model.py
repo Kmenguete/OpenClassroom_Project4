c77f6ceb8e6c1b30f272e532c2c4167ee9987b86
Kmenguete <menguetekevin@gmail.com>
2021-09-23 15:10:44 +0200

    Modèle de l'application

--- OpenClassroom_projet4/model/Match_model.py
+++ OpenClassroom_projet4/model/Match_model.py
@@ -2,9 +2,11 @@ from OpenClassroom_projet4.controller.Controller import Controller
 
 
 class Match:
-    def _init_(self, player, score_player):
-        self.player = player
-        self.score_player = score_player
+    def _init_(self, player_1, player_2, score_player_1, score_player_2):
+        self.player_1 = player_1
+        self.score_player_1 = score_player_1
+        self.player_2 = player_2
+        self.score_player_2 = score_player_2
 
 
 def controller_update():
