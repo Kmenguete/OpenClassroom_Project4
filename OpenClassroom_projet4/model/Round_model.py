1df4c4ee09ddad13d00b7b119d65f9698deccbf6
Kmenguete <menguetekevin@gmail.com>
2021-09-23 16:36:06 +0200

    Modèle de l'application

--- OpenClassroom_projet4/model/Round_model.py
+++ OpenClassroom_projet4/model/Round_model.py
@@ -2,8 +2,10 @@ from OpenClassroom_projet4.controller.Controller import Controller
 
 
 class Round:
-    def _init_(self, match):
-        self.match = match
+    def _init_(self, name_of_tournament, round_number, match_number):
+        self.name_of_tournament = name_of_tournament
+        self.round_number = round_number
+        self.match_number = match_number
 
 
 def controller_update():
