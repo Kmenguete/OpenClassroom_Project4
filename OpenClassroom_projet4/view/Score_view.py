ed358abd64431ca2a48f11e6b150213cfb41d9ff
Kmenguete <menguetekevin@gmail.com>
2021-09-23 10:27:53 +0200

    Vue de l'application

--- OpenClassroom_projet4/view/Score_view.py
+++
@@ -1,5 +0,0 @@
-
-
-class Score:
-    def score_of_player(self, match, round, tournament, rank):
-        pass
--- OpenClassroom_projet4/view/Tournament_view.py
+++ OpenClassroom_projet4/view/Tournament_view.py
@@ -1,12 +1,15 @@
+from OpenClassroom_projet4.model.Match_model import Match
+from OpenClassroom_projet4.model.Round_model import Round
+from OpenClassroom_projet4.model.Tournament_model import Tournament
 
 
-class TournamentView:
+def create_tournament():
+    return Tournament
 
-    def create_tournament(self, tournament):
-        pass
 
-    def create_round(self, round):
-        pass
+def create_round():
+    return Round
 
-    def create_match(self, match):
-        pass
+
+def create_match():
+    return Match
