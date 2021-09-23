1388485ed60531cc2356d109bc6de38219a732ba
Kmenguete <menguetekevin@gmail.com>
2021-09-23 17:05:31 +0200

    Exécution de l'application

---
+++ OpenClassroom_projet4/Execution/Round_execution.py
@@ -0,0 +1,22 @@
+
+class Round:
+    def __init__(self, name_of_tournament, round_number, match_number, score_player_1, score_player_2):
+        self.name_of_tournament = name_of_tournament
+        self.round_number = round_number
+        self.match_number = match_number
+        self.score_player_1 = score_player_1
+        self.score_player_2 = score_player_2
+
+    def __str__(self):
+        return self.name_of_tournament + " " + self.round_number + " " + self.match_number + " " + self.score_player_1 +\
+               " " + self.score_player_2
+
+
+if __name__ == '__main__':
+    name_of_tournament = input("enter the name of the tournament: ")
+    round_number = input("enter the round number: ")
+    match_number = input("enter the match number: ")
+    score_player_1 = input("enter the score of player 1: ")
+    score_player_2 = input("enter the score of player 2: ")
+    round = Round(name_of_tournament, round_number, match_number, score_player_1, score_player_2)
+    print("Here is the last information about the selected match: " + str(round))
