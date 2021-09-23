50cc8e915f024211b0e50f926ac32fcb7c929d43
Kmenguete <menguetekevin@gmail.com>
2021-09-23 11:01:46 +0200

    Modèle de l'application

--- OpenClassroom_projet4/model/Tournament_model.py
+++ OpenClassroom_projet4/model/Tournament_model.py
@@ -2,11 +2,11 @@
 
 class Tournament:
     def _init_(self, name_of_tournament, place_of_tournament, date_of_tournament, duration_of_tournament,
-               number_of_round, time_tracking, description_of_tournament):
+               number_of_round, number_of_match, description_of_tournament):
         self.name_of_tournament = name_of_tournament
         self.place_of_tournament = place_of_tournament
         self.date_of_tournament = date_of_tournament
         self.duration_of_tournament = duration_of_tournament
         self.number_of_round = number_of_round
-        self.time_tracking = time_tracking
+        self.number_of_match = number_of_match
         self.description_of_tournament = description_of_tournament
