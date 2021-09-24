559693214a32f3aa27603f2d9752db2320c5b938
Kmenguete <menguetekevin@gmail.com>
2021-09-24 13:55:41 +0200

    Exécution de l'application

---
+++ OpenClassroom_projet4/Execution/Swiss_tournament_system.py
@@ -0,0 +1,15 @@
+
+
+class SwissTournamentSystem:
+
+    def sort_player_by_rank(self):
+        pass
+
+    def split_total_number_of_players(self):
+        pass
+
+    def pairs_of_players(self):
+        pass
+
+    def sort_players_by_score_round(self):
+        pass
