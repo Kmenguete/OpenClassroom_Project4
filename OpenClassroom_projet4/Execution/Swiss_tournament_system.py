8acdc21bf413816f1e60f10de3ce3f5212816cab
Kmenguete <menguetekevin@gmail.com>
2021-09-24 14:29:42 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/Swiss_tournament_system.py
+++ OpenClassroom_projet4/Execution/Swiss_tournament_system.py
@@ -13,3 +13,8 @@ class SwissTournamentSystem:
 
     def sort_players_by_score_round(self):
         pass
+
+    def generate_new_pairs_of_players(self):
+        pass
+
+# I repeat the two last functions until the end of tournament.
