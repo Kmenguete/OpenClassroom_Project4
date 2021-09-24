2b68b5e9d1ea2d700fc511f749b927441dedcc67
Kmenguete <menguetekevin@gmail.com>
2021-09-24 15:32:53 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/Swiss_tournament_system.py
+++ OpenClassroom_projet4/Execution/Swiss_tournament_system.py
@@ -1,20 +1,26 @@
 
+def sort_player_by_rank(player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8):
+    player_list = {'2nd': player_1, '8th': player_2, '1st': player_3, '5th': player_4, '4th': player_5,
+                   '3rd': player_6, '6th': player_7, '7th': player_8}
+    sorted_player_list = sorted(player_list)
+    return sorted_player_list
+
 
 class SwissTournamentSystem:
 
-    def sort_player_by_rank(self):
+    def split_total_number_of_players(self, player_1, player_2, player_3, player_4, player_5, player_6, player_7,
+                                      player_8):
         pass
 
-    def split_total_number_of_players(self):
+    def pairs_of_players(self, player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8):
         pass
 
-    def pairs_of_players(self):
+    def sort_players_by_score_round(self, player_1, player_2, player_3, player_4, player_5, player_6, player_7,
+                                    player_8):
         pass
 
-    def sort_players_by_score_round(self):
-        pass
-
-    def generate_new_pairs_of_players(self):
+    def generate_new_pairs_of_players(self, player_1, player_2, player_3, player_4, player_5, player_6, player_7,
+                                      player_8):
         pass
 
 # I repeat the two last functions until the end of tournament.
