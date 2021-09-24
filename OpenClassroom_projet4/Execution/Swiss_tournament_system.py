8ee29f5fea357fadd1dfc69b2dfc0ef80db5f2f2
Kmenguete <menguetekevin@gmail.com>
2021-09-24 15:45:42 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/Swiss_tournament_system.py
+++ OpenClassroom_projet4/Execution/Swiss_tournament_system.py
@@ -1,7 +1,8 @@
-# My Player class instances will be store in variables player_1, player_2, player_3...etc.
-# The rank of my players are the key of my dictionary.
 
 def sort_player_by_rank(player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8):
+    # The goal of this function is to sort players by rank.
+    # My Player class instances will be store in variables player_1, player_2, player_3...etc.
+    # The rank of my players are the key of my dictionary. my dictionary will be sorted by key.
     player_list = {'2nd': player_1, '8th': player_2, '1st': player_3, '5th': player_4, '4th': player_5,
                    '3rd': player_6, '6th': player_7, '7th': player_8}
     sorted_player_list = sorted(player_list)
