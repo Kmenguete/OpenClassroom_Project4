2b4584c66e4fdbeaedbd52f5cf1eb94388341d9f
Kmenguete <menguetekevin@gmail.com>
2021-09-30 15:06:20 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/Round_execution.py
+++ OpenClassroom_projet4/Execution/Round_execution.py
@@ -1,37 +1,26 @@
-from tinydb import TinyDB
 
 
 class Round:
-    def __init__(self, name_of_tournament, round_number, match_number, player_a, player_b, score_player_a, score_player_b):
+    def __init__(self, name_of_tournament, round_number, match_1, match_2, match_3, match_4):
         self.name_of_tournament = name_of_tournament
         self.round_number = round_number
-        self.match_number = match_number
-        self.player_a = player_a
-        self.player_b = player_b
-        self.score_player_a = score_player_a
-        self.score_player_b = score_player_b
+        self.match_1 = match_1
+        self.match_2 = match_2
+        self.match_3 = match_3
+        self.match_4 = match_4
 
     def __str__(self):
-        return self.name_of_tournament + " " + self.round_number + " " + self.match_number + " " + self.player_a + " " \
-               + self.player_b + " " + self.score_player_a + " " + self.score_player_b
+        return self.name_of_tournament + " " + self.round_number + " " + self.match_1 + " " + self.match_2 + " " \
+               + self.match_3 + " " + self.match_4
 
 
 if __name__ == '__main__':
     name_of_tournament = input("enter the name of the tournament: ")
     round_number = input("enter the round number: ")
-    match_number = input("enter the match number: ")
-    player_a = input("enter the firstname and surname of player a: ")
-    player_b = input("enter the firstname and surname of player b: ")
-    score_player_a = input("enter the score of player a: ")
-    score_player_b = input("enter the score of player b: ")
-    round = Round(name_of_tournament, round_number, match_number, player_a, player_b, score_player_a,
-                  score_player_b)
-    print("Here is the last information about the selected match: " + str(round))
-    round_database = TinyDB('Round.json')
-    round_database.insert({'name_of_tournament': name_of_tournament})
-    round_database.insert({'round_number': round_number})
-    round_database.insert({'match_number': match_number})
-    round_database.insert({'player_a': player_a})
-    round_database.insert({'player_b': player_b})
-    round_database.insert({'score_player_a': score_player_a})
-    round_database.insert({'score_player_b': score_player_b})
+    match_1 = input("enter the pair of players for the match 1: ")
+    match_2 = input("enter the pair of players for the match 2: ")
+    match_3 = input("enter the pair of players for the match 3: ")
+    match_4 = input("enter the pair of players for the match 4: ")
+    round = Round(name_of_tournament, round_number, match_1, match_2, match_3, match_4)
+    print("Here is the 4 pairs of match for the current round: " + str(round))
+
