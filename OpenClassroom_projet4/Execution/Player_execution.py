9f3c72cee0cf541b32cd6ee1167f977d8934cb12
Kmenguete <menguetekevin@gmail.com>
2021-09-30 14:49:18 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/Player_execution.py
+++ OpenClassroom_projet4/Execution/Player_execution.py
@@ -2,28 +2,29 @@ from tinydb import TinyDB
 
 
 class Player:
-    def __init__(self, surname, firstname, date_of_birth, sex, rank):
-        self.surname = surname
-        self.firstname = firstname
-        self.date_of_birth = date_of_birth
-        self.sex = sex
-        self.rank = rank
+    def __init__(self, player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8):
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.player_3 = player_3
+        self.player_4 = player_4
+        self.player_5 = player_5
+        self.player_6 = player_6
+        self.player_7 = player_7
+        self.player_8 = player_8
 
     def __str__(self):
-        return self.surname + " " + self.firstname + " " + self.date_of_birth + " " + self.sex + " " + self.rank
+        return self.player_1 + " " + self.player_2 + " " + self.player_3 + " " + self.player_4 + " " + self.player_5 + \
+               " " + self.player_6 + " " + self.player_7 + " " + self.player_8
 
 
 if __name__ == '__main__':
-    surname = input("enter the surname: ")
-    firstname = input("enter the firstname: ")
-    date_of_birth = input("enter the date of birth: ")
-    sex = input("enter the sex: ")
-    rank = input("enter the rank: ")
-    player = Player(surname, firstname, date_of_birth, sex, rank)
-    print("Here is your player: " + str(player))
-    player_database = TinyDB('Player.json')
-    player_database.insert({'surname': surname})
-    player_database.insert({'firstname': firstname})
-    player_database.insert({'birthdate': date_of_birth})
-    player_database.insert({'sex': sex})
-    player_database.insert({'rank': rank})
+    player_1 = input("Enter the player 1: ")
+    player_2 = input("Enter the player 2: ")
+    player_3 = input("Enter the player 3: ")
+    player_4 = input("Enter the player 4: ")
+    player_5 = input("Enter the player 5: ")
+    player_6 = input("Enter the player 6: ")
+    player_7 = input("Enter the player 7: ")
+    player_8 = input("Enter the player 8: ")
+    player = Player(player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8)
+    print("Here is your 8 players: " + str(player))
