d85066706cca2b15083e6729578ab14296aec1b0
Kmenguete <menguetekevin@gmail.com>
2021-10-01 10:37:07 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/Player_execution.py
+++ OpenClassroom_projet4/Execution/Player_execution.py
@@ -1,29 +1,20 @@
 
 
 class Player:
-    def __init__(self, player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8):
-        self.player_1 = player_1
-        self.player_2 = player_2
-        self.player_3 = player_3
-        self.player_4 = player_4
-        self.player_5 = player_5
-        self.player_6 = player_6
-        self.player_7 = player_7
-        self.player_8 = player_8
+    def __init__(self, surname, firstname, birthdate, sex):
+        self.surname = surname
+        self.firstname = firstname
+        self.birthdate = birthdate
+        self.sex = sex
 
     def __str__(self):
-        return self.player_1 + " " + self.player_2 + " " + self.player_3 + " " + self.player_4 + " " + self.player_5 + \
-               " " + self.player_6 + " " + self.player_7 + " " + self.player_8
+        return self.surname + " " + self.firstname + " " + self.birthdate + " " + self.sex
 
 
 if __name__ == '__main__':
-    player_1 = input("Enter the player 1: ")
-    player_2 = input("Enter the player 2: ")
-    player_3 = input("Enter the player 3: ")
-    player_4 = input("Enter the player 4: ")
-    player_5 = input("Enter the player 5: ")
-    player_6 = input("Enter the player 6: ")
-    player_7 = input("Enter the player 7: ")
-    player_8 = input("Enter the player 8: ")
-    player = Player(player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8)
-    print("Here is your 8 players: " + str(player))
+    surname = input("Enter the surname: ")
+    firstname = input("Enter the firstname: ")
+    birthdate = input("Enter the birthdate: ")
+    sex = input("Enter the sex: ")
+    player = Player(surname, firstname, birthdate, sex)
+    print("You have successfully created a player: " + str(player))
--- OpenClassroom_projet4/Execution/Round_execution.py
+++ OpenClassroom_projet4/Execution/Round_execution.py
@@ -1,25 +1,18 @@
 
 
 class Round:
-    def __init__(self, name_of_tournament, round_number, match_1, match_2, match_3, match_4):
+    def __init__(self, name_of_tournament, round_number, match_list):
         self.name_of_tournament = name_of_tournament
         self.round_number = round_number
-        self.match_1 = match_1
-        self.match_2 = match_2
-        self.match_3 = match_3
-        self.match_4 = match_4
+        self.match_list = match_list
 
     def __str__(self):
-        return self.name_of_tournament + " " + self.round_number + " " + self.match_1 + " " + self.match_2 + " " \
-               + self.match_3 + " " + self.match_4
+        return self.name_of_tournament + " " + self.round_number + " " + self.match_list
 
 
 if __name__ == '__main__':
     name_of_tournament = input("enter the name of the tournament: ")
     round_number = input("enter the round number: ")
-    match_1 = input("enter the pair of players for the match 1: ")
-    match_2 = input("enter the pair of players for the match 2: ")
-    match_3 = input("enter the pair of players for the match 3: ")
-    match_4 = input("enter the pair of players for the match 4: ")
-    round = Round(name_of_tournament, round_number, match_1, match_2, match_3, match_4)
-    print("Here is the 4 pairs of match for the current round: " + str(round))
+    match_list = input("enter the randomly generated matches: ")
+    round = Round(name_of_tournament, round_number, match_list)
+    print("Here are the matches for the current round: " + str(round))
