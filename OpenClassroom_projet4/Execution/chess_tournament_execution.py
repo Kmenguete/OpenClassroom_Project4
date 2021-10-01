8b72ac9440b4df124b109095334c35a1619a14a5
Kmenguete <menguetekevin@gmail.com>
2021-10-01 11:37:50 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/chess_tournament_execution.py
+++ OpenClassroom_projet4/Execution/chess_tournament_execution.py
@@ -1,6 +1,7 @@
 from OpenClassroom_projet4.Execution.Match_execution import Match
 from OpenClassroom_projet4.Execution.Player_execution import Player
 from OpenClassroom_projet4.Execution.Round_execution import Round
+from OpenClassroom_projet4.Execution.Swiss_tournament_system import pairs_of_players
 from OpenClassroom_projet4.Execution.Tournament_execution import Tournament
 
 if __name__ == '__main__':
@@ -20,64 +21,67 @@ if __name__ == '__main__':
     firstname = input("Enter the firstname: ")
     birthdate = input("Enter the birthdate: ")
     sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 1: " + str(player))
-
-if __name__ == '__main__':
-    surname = input("Enter the surname: ")
-    firstname = input("Enter the firstname: ")
-    birthdate = input("Enter the birthdate: ")
-    sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 2: " + str(player))
-
-if __name__ == '__main__':
-    surname = input("Enter the surname: ")
-    firstname = input("Enter the firstname: ")
-    birthdate = input("Enter the birthdate: ")
-    sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 3: " + str(player))
-
-if __name__ == '__main__':
-    surname = input("Enter the surname: ")
-    firstname = input("Enter the firstname: ")
-    birthdate = input("Enter the birthdate: ")
-    sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 4: " + str(player))
-
-if __name__ == '__main__':
-    surname = input("Enter the surname: ")
-    firstname = input("Enter the firstname: ")
-    birthdate = input("Enter the birthdate: ")
-    sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 5: " + str(player))
-
-if __name__ == '__main__':
-    surname = input("Enter the surname: ")
-    firstname = input("Enter the firstname: ")
-    birthdate = input("Enter the birthdate: ")
-    sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 6: " + str(player))
-
-if __name__ == '__main__':
-    surname = input("Enter the surname: ")
-    firstname = input("Enter the firstname: ")
-    birthdate = input("Enter the birthdate: ")
-    sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 7: " + str(player))
-
-if __name__ == '__main__':
-    surname = input("Enter the surname: ")
-    firstname = input("Enter the firstname: ")
-    birthdate = input("Enter the birthdate: ")
-    sex = input("Enter the sex: ")
-    player = Player(surname, firstname, birthdate, sex)
-    print("You have successfully created the player 8: " + str(player))
+    player_a = Player(surname, firstname, birthdate, sex)
+    print("You have successfully created the player 1: " + str(player_a))
+    if __name__ == '__main__':
+        surname = input("Enter the surname: ")
+        firstname = input("Enter the firstname: ")
+        birthdate = input("Enter the birthdate: ")
+        sex = input("Enter the sex: ")
+        player_b = Player(surname, firstname, birthdate, sex)
+        print("You have successfully created the player 2: " + str(player_b))
+        if __name__ == '__main__':
+            surname = input("Enter the surname: ")
+            firstname = input("Enter the firstname: ")
+            birthdate = input("Enter the birthdate: ")
+            sex = input("Enter the sex: ")
+            player_c = Player(surname, firstname, birthdate, sex)
+            print("You have successfully created the player 3: " + str(player_c))
+            if __name__ == '__main__':
+                surname = input("Enter the surname: ")
+                firstname = input("Enter the firstname: ")
+                birthdate = input("Enter the birthdate: ")
+                sex = input("Enter the sex: ")
+                player_d = Player(surname, firstname, birthdate, sex)
+                print("You have successfully created the player 4: " + str(player_d))
+                if __name__ == '__main__':
+                    surname = input("Enter the surname: ")
+                    firstname = input("Enter the firstname: ")
+                    birthdate = input("Enter the birthdate: ")
+                    sex = input("Enter the sex: ")
+                    player_e = Player(surname, firstname, birthdate, sex)
+                    print("You have successfully created the player 5: " + str(player_e))
+                    if __name__ == '__main__':
+                        surname = input("Enter the surname: ")
+                        firstname = input("Enter the firstname: ")
+                        birthdate = input("Enter the birthdate: ")
+                        sex = input("Enter the sex: ")
+                        player_f = Player(surname, firstname, birthdate, sex)
+                        print("You have successfully created the player 6: " + str(player_f))
+                        if __name__ == '__main__':
+                            surname = input("Enter the surname: ")
+                            firstname = input("Enter the firstname: ")
+                            birthdate = input("Enter the birthdate: ")
+                            sex = input("Enter the sex: ")
+                            player_g = Player(surname, firstname, birthdate, sex)
+                            print("You have successfully created the player 7: " + str(player_g))
+                            if __name__ == '__main__':
+                                surname = input("Enter the surname: ")
+                                firstname = input("Enter the firstname: ")
+                                birthdate = input("Enter the birthdate: ")
+                                sex = input("Enter the sex: ")
+                                player_h = Player(surname, firstname, birthdate, sex)
+                                print("You have successfully created the player 8: " + str(player_h))
+                                player_1 = player_a.firstname + " " + player_a.surname
+                                player_2 = player_b.firstname + " " + player_b.surname
+                                player_3 = player_c.firstname + " " + player_c.surname
+                                player_4 = player_d.firstname + " " + player_d.surname
+                                player_5 = player_e.firstname + " " + player_e.surname
+                                player_6 = player_f.firstname + " " + player_f.surname
+                                player_7 = player_g.firstname + " " + player_g.surname
+                                player_8 = player_h.firstname + " " + player_h.surname
+                                pairs_of_players(player_1, player_2, player_3, player_4, player_5, player_6, player_7,
+                                                 player_8)
 
 if __name__ == '__main__':
     name_of_tournament = input("enter the name of the tournament: ")
