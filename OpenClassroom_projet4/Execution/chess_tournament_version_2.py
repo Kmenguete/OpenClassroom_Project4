7447d7e4dab897496a62310737f4ce083bf2f03a
Kmenguete <menguetekevin@gmail.com>
2021-10-09 15:28:37 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/chess_tournament_version_2.py
+++ OpenClassroom_projet4/Execution/chess_tournament_version_2.py
@@ -14,22 +14,23 @@ if __name__ == '__main__':
     tournament = Tournament(name_of_tournament, place_of_tournament, date_of_tournament, duration_of_tournament,
                             number_of_round, number_of_match, description_of_tournament)
     print("You have successfully created the following tournament: " + str(tournament))
-    number_players = int(input("Enter the number of players: "))
-    player_list = []
-    for player in range(number_players):
-        if number_players == 1:
-            print("There is not enough players to play a tournament.")
-        else:
-            surname = input("Enter the surname: ")
-            firstname = input("Enter the firstname: ")
-            birthdate = input("Enter the birthdate: ")
-            sex = input("Enter the sex: ")
-            rank = input("enter the rank of the player: ")
-            player = Player(surname, firstname, birthdate, sex, rank)
-            print("You have successfully created a player: " + str(player))
-        player_list.append(player)
-    pairs_of_players(player_list)
 
+number_players = int(input("Enter the number of players: "))
+player_list = []
+for player in range(number_players):
+    if number_players == 1:
+        print("There is not enough players to play a tournament.")
+    else:
+        surname = input("Enter the surname: ")
+        firstname = input("Enter the firstname: ")
+        birthdate = input("Enter the birthdate: ")
+        sex = input("Enter the sex: ")
+        rank = input("enter the rank of the player: ")
+        player = Player(surname, firstname, birthdate, sex, rank)
+        print("You have successfully created a player: " + str(player))
+    player_list.append(player)
+
+pairs_of_players(player_list)
 
 number_rounds = int(input("Enter the number of rounds: "))
 round_list = []
@@ -102,4 +103,5 @@ for round in range(number_rounds):
                               score_player_b)
                 print("Here is the result of the match: " + str(match))
             match_list.append(match)
+    pairs_of_players(player_list)
     round_list.append(round)
