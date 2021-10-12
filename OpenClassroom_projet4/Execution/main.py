9996806836781f31c857af121f0e40b610118c67
Kmenguete <menguetekevin@gmail.com>
2021-10-12 15:14:09 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/main.py
+++ OpenClassroom_projet4/Execution/main.py
@@ -73,27 +73,32 @@ if __name__ == '__main__':
         # for the next round player 1 meet player 2 and player 3 meet player 4 and so on.
         # If the player 1 have already met the player 2 then he meet the player 3. If the player 3 have already
         # met the player 4 then he meet the player 5 and so on.
+        match_list = []
         odd_players = []
         even_players = []
         for player in range(0, len(tournament.players)):
-            if player % 2:
+            if player % 2 == 0:
                 even_players.append(tournament.players[player])
             else:
                 odd_players.append(tournament.players[player])
                 new_player_pairs = list(list(x) for x in zip(odd_players, even_players))
                 print(tuple(new_player_pairs))
+                    
                 for new_player_pair in new_player_pairs:
                     print("creating match")
                     match = Match(new_player_pair[0], new_player_pair[1])
                     match_list.append(match)
-                    if new_player_pair in round.matches:
+                    i = 0
+                    if new_player_pair in tournament.rounds[i].matches:
                         print("A match with these players already exist. Please, promote the following match: ")
                         alternative_matches = [(player, (player + 1) % len(odd_players))
                                                for player in range(len(odd_players))]
                         alternative_matches_2 = [(player, (player + 1) % len(even_players))
                                                  for player in range(len(even_players))]
-                        print(tuple(alternative_matches))
-                        print(tuple(alternative_matches_2))
+                        for match in alternative_matches:
+                            print(match)
+                        for match in alternative_matches_2:
+                            print(match)
                         for alternative_match in alternative_matches:
                             print("creating match")
                             match = Match(alternative_match[0], alternative_match[1])
@@ -104,25 +109,24 @@ if __name__ == '__main__':
                             match_list.append(match)
                     else:
                         print("No previous match found with these players")
-
-                i = 1
-                next_round = Tour("Round {}".format(i), datetime.now(), match_list)
-                round_list = [next_round]
-                tournament.rounds = round_list
-                i += 1
-
-                for match in tournament.rounds[i].matches:
-                    print("Player A: " + match.player_a.last_name)
-                    print("Player B: " + match.player_b.last_name)
-                    while True:
-                        winner = input("Enter winner (A or B): ")
-                        if winner != 'A' and winner != 'B':
-                            print("Invalid value for player, please enter either A or B")
-                        else:
-                            if winner == 'A':
-                                match.score_player_a = 1
-                                match.score_player_b = 0
-                            elif winner == 'B':
-                                match.score_player_b = 1
-                                match.score_player_a = 0
-                            break
+                        i += 1
+        i = 1
+        next_round = Tour("Round {}".format(i), datetime.now(), match_list)
+        round_list = [first_round, next_round]
+        tournament.rounds = round_list
+        for match in tournament.rounds[i].matches:
+            print("Player A: " + match.player_a.last_name)
+            print("Player B: " + match.player_b.last_name)
+            while True:
+                winner = input("Enter winner (A or B): ")
+                if winner != 'A' and winner != 'B':
+                    print("Invalid value for player, please enter either A or B")
+                else:
+                    if winner == 'A':
+                        match.score_player_a = 1
+                        match.score_player_b = 0
+                    elif winner == 'B':
+                        match.score_player_b = 1
+                        match.score_player_a = 0
+                    break
+            i += 1
