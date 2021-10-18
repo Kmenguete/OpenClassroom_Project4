284d3d7f605a958766386094857d114be25a45ac
Kmenguete <menguetekevin@gmail.com>
2021-10-18 11:03:06 +0200

    Modèle de l'application

--- OpenClassroom_projet4/model/Tournament_model.py
+++ OpenClassroom_projet4/model/Tournament_model.py
@@ -1,10 +1,8 @@
-from OpenClassroom_projet4.model.Round_model import Tour
-
 DEFAULT_ROUNDS_NUMBER = 4
 
 
 class Tournament:
-    def __init__(self, name, place, date, description, number_of_rounds=DEFAULT_ROUNDS_NUMBER, rounds: Tour = None,
+    def __init__(self, name, place, date, description, number_of_rounds=DEFAULT_ROUNDS_NUMBER, rounds=None,
                  players=None):
         self.name = name
         self.place = place
@@ -19,3 +17,19 @@ class Tournament:
 
     def __getitem__(self, players, i):
         return self.players[i]
+
+    def check_if_match_already_happened(self, player_a, player_b):
+        already_happened = False
+        for tour in self.rounds:
+            for match in tour.matches:
+                if (match.player_a == player_a and match.player_b == player_b) or (match.player_a == player_b and
+                                                                                   match.player_b == player_a):
+                    already_happened = True
+        return already_happened
+
+    def get_next_available_player(self, player_a, next_player_index, non_available_players):
+        if self.check_if_match_already_happened(player_a, self.players[next_player_index]) or \
+                self.players[next_player_index] in non_available_players:
+            self.get_next_available_player(player_a, next_player_index + 1, non_available_players)
+        else:
+            return self.players[next_player_index]
