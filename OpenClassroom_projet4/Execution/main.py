76b9ee204aae765e9083a5048ed428069525c7ee
Kmenguete <menguetekevin@gmail.com>
2021-10-22 15:40:12 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/main.py
+++ OpenClassroom_projet4/Execution/main.py
@@ -48,6 +48,10 @@ if __name__ == '__main__':
     first_round = Tour("Round 1", datetime.now(), match_list)
     round_list = [first_round]
     tournament.rounds = round_list
+    tournament.players = sorted_player_list
+    tournament.players_dict = tournament.create_list_dict()
+    print(tournament.players_dict)
+
     # End of step 3
 
     # Step 4: Enter the result of the first round
@@ -69,49 +73,15 @@ if __name__ == '__main__':
                 elif winner == 'None':
                     match.score_player_b = 0.5
                     match.score_player_a = 0.5
+                    tournament.seek_player_and_update_score(match.player_a.rank, match.score_player_a)
+                    tournament.seek_player_and_update_score(match.player_b.rank, match.score_player_b)
                 break
 
-    # Step 5: we update the rank of each player according their scores.
-    winner_list = []
-    loser_list = []
-    none_winner_loser_list = []
-    new_player_list = []
-    n = 0
-    for h in range(0, len(tournament.rounds[0].matches)):
-        if tournament.players[n] == tournament.rounds[0].matches[h].player_a and \
-                tournament.rounds[0].matches[h].score_player_a == 1:
-            winner_list.append(tournament.players[n])
-        elif tournament.players[n] == tournament.rounds[0].matches[h].player_a and \
-                tournament.rounds[0].matches[h].score_player_a == 0.5:
-            none_winner_loser_list.append(tournament.players[n])
-        else:
-            loser_list.append(tournament.players[n])
-        n += 1
+    # Step 5: we sort players by their score.
 
-    for h in range(0, len(tournament.rounds[0].matches)):
-        if tournament.players[n] == tournament.rounds[0].matches[h].player_b and \
-                tournament.rounds[0].matches[h].score_player_b == 1:
-            winner_list.append(tournament.players[n])
-        elif tournament.players[n] == tournament.rounds[0].matches[h].player_b and \
-                tournament.rounds[0].matches[h].score_player_b == 0.5:
-            none_winner_loser_list.append(tournament.players[n])
-        else:
-            loser_list.append(tournament.players[n])
-        n += 1
-
-    sorted_winner_list = sorted(winner_list, key=operator.attrgetter("rank"))
-    sorted_none_winner_loser_list = sorted(none_winner_loser_list, key=operator.attrgetter("rank"))
-    sorted_loser_list = sorted(loser_list, key=operator.attrgetter("rank"))
-    new_player_list = sorted_winner_list + sorted_none_winner_loser_list + sorted_loser_list
-    print("Here the rank is updated according the score of each player.")
-    tournament.players = new_player_list
-    for i in range(0, len(tournament.players)):
-        tournament.players[i].update_rank(tournament.players, i)
-
-    print(tournament.players)
-    # Step 6: we generate new matches according the rank of each player at the end of each round.
-    # Step 7: When a round is finished, we update rank according score of each player in each match.
-    # We repeat the step 6 and the step 7 until the end of the tournament.
+    sorted_list_by_total_score = sorted(tournament.players_dict.items(), key=lambda x: x[1], reverse=True)
+    tournament.players_dict = sorted_list_by_total_score
+    print(tournament.players_dict)
 
     for f in range(1, tournament.number_of_rounds):
         non_available_players = []
