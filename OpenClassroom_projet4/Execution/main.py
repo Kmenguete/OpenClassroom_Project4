22c35ffdbde94204079fc4f7ebb4a72e3f921000
Kmenguete <menguetekevin@gmail.com>
2021-10-26 17:34:47 +0200

    Exécution de l'application

--- OpenClassroom_projet4/Execution/main.py
+++ OpenClassroom_projet4/Execution/main.py
@@ -88,12 +88,22 @@ if __name__ == '__main__':
             if player_a in non_available_players:
                 pass
             else:
-                player_b = tournament.get_next_available_player(player_a, index_player + 1, non_available_players)
-                non_available_players.append(player_a)
-                non_available_players.append(player_b)
-                match = Match(player_a, player_b)
-                match_list.append(match)
-                print("creating match")
+                alternative_index = 0
+                if index_player + 1 < len(tournament.players):
+                    player_b = tournament.get_next_available_player(player_a, index_player + 1, non_available_players)
+                    non_available_players.append(player_a)
+                    non_available_players.append(player_b)
+                    match = Match(player_a, player_b)
+                    match_list.append(match)
+                    print("creating match")
+                else:
+                    player_b = tournament.get_next_available_player(player_a, alternative_index, non_available_players)
+                    non_available_players.append(player_a)
+                    non_available_players.append(player_b)
+                    match = Match(player_a, player_b)
+                    match_list.append(match)
+                    print("creating match")
+                    alternative_index += 1
 
         next_round = Tour("Round {}".format(index + 1), datetime.now(), match_list)
         tournament.rounds.append(next_round)
