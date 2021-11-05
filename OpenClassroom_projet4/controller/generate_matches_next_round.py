f195e9335656c99ecc9fb0c2cc65919751b43385
Kmenguete <menguetekevin@gmail.com>
2021-11-05 16:40:28 +0100

    Contrôleur de l'application

--- OpenClassroom_projet4/controller/generate_matches_next_round.py
+++ OpenClassroom_projet4/controller/generate_matches_next_round.py
@@ -1,24 +1,22 @@
 from OpenClassroom_projet4.model.Match_model import Match
-from OpenClassroom_projet4.model.Tournament_model import DEFAULT_ROUNDS_NUMBER
 
 
 def generate_matches_next_round(player_list, tournament):
-    for index in range(1, DEFAULT_ROUNDS_NUMBER):
-        non_available_players = []
-        match_list = []
-        for index_player in range(len(player_list) - 1):
-            player_a = player_list[index_player]
-            if player_a in non_available_players:
-                pass
-            else:
-                try:
-                    player_b = tournament.get_next_available_player(player_a, index_player + 1, non_available_players)
-                    non_available_players.append(player_a)
-                    non_available_players.append(player_b)
-                    match = Match(player_a, player_b)
-                    match_list.append(match)
-                    match.display_match()
-                except IndexError:
-                    print("Unable to get player B as opponent of player A: " + player_a.firstname + " " +
-                          player_a.last_name)
-        return match_list
+    non_available_players = []
+    match_list = []
+    for index_player in range(len(player_list) - 1):
+        player_a = player_list[index_player]
+        if player_a in non_available_players:
+            pass
+        else:
+            try:
+                player_b = tournament.get_next_available_player(player_a, index_player + 1, non_available_players)
+                non_available_players.append(player_a)
+                non_available_players.append(player_b)
+                match = Match(player_a, player_b)
+                match_list.append(match)
+                match.display_match()
+            except IndexError:
+                print("Unable to get player B as opponent of player A: " + player_a.firstname + " " +
+                      player_a.last_name)
+    return match_list
