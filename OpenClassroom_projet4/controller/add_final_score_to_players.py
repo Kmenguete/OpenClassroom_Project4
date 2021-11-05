ee822aa0a09e140db4bb803d5028ae27970cfc66
Kmenguete <menguetekevin@gmail.com>
2021-11-05 17:40:39 +0100

    Contrôleur de l'application

--- OpenClassroom_projet4/controller/add_final_score_to_players.py
+++ OpenClassroom_projet4/controller/add_final_score_to_players.py
@@ -3,4 +3,3 @@
 def add_final_score_to_players(player_list, players_dict):
     for player_index in range(0, len(player_list)):
         player_list[player_index].total_score = players_dict[player_list[player_index].player_id]
-    return players_dict
