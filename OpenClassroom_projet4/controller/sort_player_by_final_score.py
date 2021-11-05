3296d56be1d3c90c6da66677b3c2f23d362ea15e
Kmenguete <menguetekevin@gmail.com>
2021-11-05 17:41:49 +0100

    Contrôleur de l'application

--- OpenClassroom_projet4/controller/sort_player_by_final_score.py
+++ OpenClassroom_projet4/controller/sort_player_by_final_score.py
@@ -3,5 +3,4 @@ import operator
 
 def sort_player_by_final_score(player_list):
     sorted_player_list = sorted(player_list, key=operator.attrgetter("total_score"), reverse=True)
-    player_list = sorted_player_list
-    return player_list
+    return sorted_player_list
