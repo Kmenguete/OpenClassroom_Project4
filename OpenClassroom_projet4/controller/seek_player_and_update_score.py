901777639541779eff15a1934480c0b0ea09d6be
Kmenguete <menguetekevin@gmail.com>
2021-11-06 13:58:28 +0100

    Contrôleur de l'application

---
+++ OpenClassroom_projet4/controller/seek_player_and_update_score.py
@@ -0,0 +1,5 @@
+
+
+def seek_player_and_update_score(player_a, player_b, score_player_a, score_player_b, tournament):
+    tournament.seek_player_and_update_score(player_a, score_player_a)
+    tournament.seek_player_and_update_score(player_b, score_player_b)
