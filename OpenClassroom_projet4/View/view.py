3b2053349ae35e9057e821e407611acfc23ab678
Kmenguete <menguetekevin@gmail.com>
2021-11-08 10:35:18 +0100

    Vue de l'application

---
+++ OpenClassroom_projet4/View/view.py
@@ -0,0 +1,42 @@
+# 1 - Display information
+# 2 - Collect information
+
+class View:
+    @staticmethod
+    def display_text(text_to_display):
+        print(text_to_display)
+
+    @staticmethod
+    def get_choice(input_text):
+        return input(input_text)
+
+    @staticmethod
+    def get_tournament_information():
+        print("************** Welcome to Kevin's Chess tournament ************* \n ")
+        name_choice = input("enter the name of tournament: ")
+        place_choice = input("enter the place of tournament: ")
+        description_choice = input("enter a description for this tournament: ")
+        print("\n ************** Tournament created ************* ")
+        return name_choice, place_choice, description_choice
+    # TODO do not forget to auto generate the date choice
+
+    @staticmethod
+    def get_player_information():
+        print("\n ***************** Register a new player *****************")
+        first_name_choice = input("Please enter the player's first name: ")
+        last_name_choice = input("Please enter the player's last name: ")
+        return first_name_choice, last_name_choice
+
+    @staticmethod
+    def display_match_information(match):
+        return "Match: " + match.player_a.firstname + " " + match.player_a.last_name + " vs " + match.player_b.firstname + \
+               " " + match.player_b.last_name
+
+    @staticmethod
+    def display_opponents(player_a, player_b):
+        print("Player A: " + player_a.firstname + " " + player_a.last_name)
+        print("Player B: " + player_b.firstname + " " + player_b.last_name)
+
+    @staticmethod
+    def display_players(players_dict):
+        print(players_dict)
--- OpenClassroom_projet4/view/Player_view.py
+++
@@ -1,5 +0,0 @@
-from OpenClassroom_projet4.model.Player_model import Player
-
-
-def create_player():
-    return Player
