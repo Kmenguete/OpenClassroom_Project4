17ca2e50bedbc5fc9762cd71fd988c646308a468
Kmenguete <menguetekevin@gmail.com>
2021-11-08 15:34:36 +0100

    Services de l'application

--- OpenClassroom_projet4/services/match_service.py
+++ OpenClassroom_projet4/services/match_service.py
@@ -17,10 +17,9 @@ class MatchService:
     def update_match_list(self, match_list):
         self.match_list = match_list
 
-    @staticmethod
-    def generate_matches_next_round(player_list, tournament_service):
+    def generate_matches_for_next_round(self, player_list, tournament_service):
         non_available_players = []
-        match_list = []
+        self.match_list = []
         for index_player in range(len(player_list) - 1):
             player_a = player_list[index_player]
             if player_a in non_available_players:
@@ -32,9 +31,9 @@ class MatchService:
                     non_available_players.append(player_a)
                     non_available_players.append(player_b)
                     match = Match(player_a, player_b)
-                    match_list.append(match)
+                    self.match_list.append(match)
                     match.display_match()
                 except IndexError:
                     print("Unable to get player B as opponent of player A: " + player_a.firstname + " " +
                           player_a.last_name)
-        return match_list
+        return self.match_list
