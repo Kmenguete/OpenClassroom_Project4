f3aa4b9f704b7645159042e2d21e59dcaaf9855b
Kmenguete <menguetekevin@gmail.com>
2021-11-08 11:54:17 +0100

    Contrôleur de l'application

--- OpenClassroom_projet4/controller/controller.py
+++ OpenClassroom_projet4/controller/controller.py
@@ -1,9 +1,11 @@
+from OpenClassroom_projet4.services.match_service import MatchService
 from OpenClassroom_projet4.services.player_service import PlayerService
+from OpenClassroom_projet4.services.tournament_service import TournamentService
 
 
 class MainController:
 
     def __init__(self):
         self.player_service = PlayerService()
-        self.match_service = None
-        self.tournament_service = None
+        self.match_service = MatchService()
+        self.tournament_service = TournamentService()
