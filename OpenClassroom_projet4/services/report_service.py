e3c083d2a6ff331ebbacaf60443d411814bc79ee
Kmenguete <menguetekevin@gmail.com>
2021-11-10 15:38:21 +0100

    Services de l'application

--- OpenClassroom_projet4/services/report_service.py
+++ OpenClassroom_projet4/services/report_service.py
@@ -9,9 +9,12 @@ class ReportService:
 
     def get_sorted_player_list_alphabetically(self, player_list):
         self.player_list = player_list.sort()
+        print(player_list)
+        return player_list
 
     def get_sorted_player_list_by_rank(self):
         sorted_player_list = sorted(self.player_list, key=operator.attrgetter("rank"))
+        print(sorted_player_list)
         return sorted_player_list
 
     def get_tournaments_list(self, tournaments_list):
