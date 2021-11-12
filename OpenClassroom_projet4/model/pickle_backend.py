6de448ba976381c049aab83cbf68d6afc6be899c
Kmenguete <menguetekevin@gmail.com>
2021-11-12 10:57:18 +0100

    Modèle de l'application

---
+++ OpenClassroom_projet4/model/pickle_backend.py
@@ -0,0 +1,27 @@
+import pickle
+
+
+class DataBase:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def save_tournament_data(tournament, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(tournament, file, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def save_players_data(players, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(players, file, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def save_matches_data(matches, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(matches, file, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def save_rounds_data(rounds, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(rounds, file, pickle.HIGHEST_PROTOCOL)
