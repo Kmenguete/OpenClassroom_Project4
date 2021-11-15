5734f311500d0b9f2a8849fee8d0b1d71203f755
Kmenguete <menguetekevin@gmail.com>
2021-11-15 15:17:39 +0100

    Ajout d'un id pour les tournois au niveau du modèle

--- OpenClassroom_projet4/model/Tournament_model.py
+++ OpenClassroom_projet4/model/Tournament_model.py
@@ -1,3 +1,5 @@
+import uuid
+
 DEFAULT_ROUNDS_NUMBER = 4
 
 
@@ -9,16 +11,31 @@ def create_list_dict(players_list):
 
 
 class Tournament:
-    def __init__(self, name, place, date, description, number_of_rounds=DEFAULT_ROUNDS_NUMBER, rounds=None,
-                 players=None, players_dict=None):
+    def __init__(self, name, place, date, description, number_of_rounds=DEFAULT_ROUNDS_NUMBER, tournament_id=None,
+                 rounds=None, players=None, players_dict=None):
         self.name = name
         self.place = place
         self.date = date
         self.number_of_rounds = number_of_rounds
+        self.tournament_id = self.get_tournament_id(tournament_id)
         self.rounds = rounds
         self.players = players
         self.players_dict = players_dict
         self.description = description
+
+    @staticmethod
+    def get_tournament_id(tournament_id):
+        if tournament_id is None:
+            tournament_id = str(uuid.uuid4())
+            return tournament_id
+
+    def __str__(self):
+        return "name: " + self.name + ", " + "place: " + self.place + ", " + "description: " + self.description + ", " \
+               + " number_of_rounds: " + str(self.number_of_rounds) + ", " + "tournament_id: " + self.tournament_id
+
+    def __repr__(self):
+        return "name: " + self.name + ", " + "place: " + self.place + ", " + "description: " + self.description + ", " \
+               + " number_of_rounds: " + str(self.number_of_rounds) + ", " + "tournament_id: " + self.tournament_id
 
     def __len__(self):
         return len(self.players)
