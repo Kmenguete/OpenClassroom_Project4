9cf17e103b89b932c2d74972e3d6bdffa0b1dd1e
Kmenguete <menguetekevin@gmail.com>
2021-11-15 16:58:22 +0100

    Ajout d'une représentation lisible de l'objet tour

--- OpenClassroom_projet4/model/Round_model.py
+++ OpenClassroom_projet4/model/Round_model.py
@@ -10,3 +10,11 @@ class Tour:
 
     def set_matches(self, matches):
         self.matches = matches
+
+    def __str__(self):
+        return "round_name: " + self.round_name + ", " + "start_date: " + str(self.start_date) + ", " + "match_list: " \
+               + str(self.matches)
+
+    def __repr__(self):
+        return "round_name: " + self.round_name + ", " + "start_date: " + str(self.start_date) + ", " + "match_list: " \
+               + str(self.matches)
