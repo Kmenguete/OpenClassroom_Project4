def473e11242590691e1526a60d2ed1a2de422de
Kmenguete <menguetekevin@gmail.com>
2021-11-15 11:41:16 +0100

    Problème du player_id pour la désérialisation résolut et pas de nouveaux bug généré pour le dictionnaire des joueurs

--- OpenClassroom_projet4/model/Player_model.py
+++ OpenClassroom_projet4/model/Player_model.py
@@ -3,15 +3,20 @@ import uuid
 
 class Player:
 
-    def __init__(self, last_name, firstname, rank, total_score=0, date_of_birth=None,
-                 sex=None):
-        self.player_id = str(uuid.uuid4())
+    def __init__(self, last_name, firstname, rank, total_score=0, player_id=None, date_of_birth=None, sex=None):
+        self.player_id = self.get_player_id(player_id)
         self.last_name = last_name
         self.firstname = firstname
         self.date_of_birth = date_of_birth
         self.sex = sex
         self.rank = int(rank)
         self.total_score = total_score
+
+    @staticmethod
+    def get_player_id(player_id):
+        if player_id is None:
+            player_id = str(uuid.uuid4())
+            return player_id
 
     def __getitem__(self, player):
         player = self.firstname + " " + self.last_name + " " + "rank: " + str(self.rank) + ", " + "score: " + \
