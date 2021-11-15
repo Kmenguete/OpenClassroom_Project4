ed89c1038096b4cf6ce91b0fb18370cb78692df1
Kmenguete <menguetekevin@gmail.com>
2021-11-15 13:58:10 +0100

    Pour éviter toutes confusion, je commente ces bases de données

--- OpenClassroom_projet4/model/tinydb_backend.py
+++ OpenClassroom_projet4/model/tinydb_backend.py
@@ -1,10 +1,10 @@
-from tinydb import TinyDB
 
 
 class DataBase:
+    pass
 
-    def __init__(self):
-        self.tournament_database = TinyDB('tournament_database.json')
-        self.players_database = TinyDB('players_database.json')
-        self.rounds_database = TinyDB('rounds_database.json')
-        self.matches_database = TinyDB('matches_database.json')
+    # def __init__(self):
+    #     self.tournament_database = TinyDB('tournament_database.json')
+    #     self.players_database = TinyDB('players_database.json')
+    #     self.rounds_database = TinyDB('rounds_database.json')
+    #     self.matches_database = TinyDB('matches_database.json')
