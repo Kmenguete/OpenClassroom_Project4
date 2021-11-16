7ae40ff234dcf40cc9ffbe3fd786051ecb0ca501
Kmenguete <menguetekevin@gmail.com>
2021-11-16 11:17:40 +0100

    Intégration d'un match_id dans la base de données

--- OpenClassroom_projet4/database/match_serializer.py
+++ OpenClassroom_projet4/database/match_serializer.py
@@ -5,11 +5,13 @@ class MatchSerializer:
 
     @staticmethod
     def serialize(match: Match):
-        serialized_match = {'player_a': match.player_a, 'score_player_a': match.score_player_a,
-                            'player_b': match.player_b, 'score_player_b': match.score_player_b}
+        serialized_match = {'match_id': match.match_id, 'player_a': match.player_a,
+                            'score_player_a': match.score_player_a, 'player_b': match.player_b,
+                            'score_player_b': match.score_player_b}
         return serialized_match
 
     @staticmethod
     def deserialize(serialized_match: dict):
-        return Match(player_a=serialized_match['player_a'], score_player_a=serialized_match['score_player_a'],
-                     player_b=serialized_match['player_b'], score_player_b=serialized_match['score_player_b'])
+        return Match(match_id=serialized_match['match_id'], player_a=serialized_match['player_a'],
+                     score_player_a=serialized_match['score_player_a'], player_b=serialized_match['player_b'],
+                     score_player_b=serialized_match['score_player_b'])
--- OpenClassroom_projet4/database/match_table.py
+++ OpenClassroom_projet4/database/match_table.py
@@ -14,9 +14,9 @@ class MatchTable:
         serialized_match = self.match_serializer.serialize(match)
         self.match_database.insert(serialized_match)
 
-    def get_match(self, player_a):
+    def get_match(self, match_id):
         match_query = Query()
-        serialized_match = self.match_database.search(match_query.player_a == player_a)
+        serialized_match = self.match_database.search(match_query.match_id == match_id)
         match = self.match_serializer.deserialize(serialized_match[0])
         return match
 
@@ -35,15 +35,19 @@ class MatchTable:
             matches.append(match)
         return matches
 
-    def update_match(self, player_a, player_b):
+    def update_match(self, match_id):
         match = Query()
-        self.match_database.update({'player_a': player_a}, match.player_b == player_b)
+        self.match_database.update({'match_id': match_id}, match.match_id == match_id)
+
+    def clear_database(self):
+        self.match_database.truncate()
 
 
 if __name__ == '__main__':
-    match_1 = Match(player_a='Samantha Paraba', score_player_a=0, player_b='Daniel Pepeou', score_player_b=1)
-    match_2 = Match(player_a='Sarah Heynes', score_player_a=0.5, player_b='Rosalie Seicheine', score_player_b=0.5)
+    match_1 = Match(match_id='Q7STS76FS87G', player_a='Samantha Paraba', score_player_a=0, player_b='Daniel Pepeou',
+                    score_player_b=1)
+    match_2 = Match(match_id='Q7STS76FS87G', player_a='Sarah Heynes', score_player_a=0.5, player_b='Rosalie Seicheine',
+                    score_player_b=0.5)
     MatchTable().save_matches([match_1, match_2])
     dsm = MatchTable().get_matches()
     print(dsm)
-
