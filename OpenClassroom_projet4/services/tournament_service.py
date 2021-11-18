423b9688dc2ab728c1abc3dd3ef8be33a4e51492
Kmenguete <menguetekevin@gmail.com>
2021-11-18 15:02:52 +0100

    I call methods that save tournament data into database

--- OpenClassroom_projet4/services/tournament_service.py
+++ OpenClassroom_projet4/services/tournament_service.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from OpenClassroom_projet4.database.round_table import RoundTable
+from OpenClassroom_projet4.database.tournament_table import TournamentTable
 from OpenClassroom_projet4.model.Round_model import Tour
 from OpenClassroom_projet4.model.Tournament_model import Tournament
 
@@ -8,19 +10,26 @@ class TournamentService:
 
     def __init__(self):
         self.tournament = None
+        self.tournament_table = TournamentTable()
+        self.round_table = RoundTable()
 
     def update_tournament(self, name, place, date, description, number_of_rounds):
         self.tournament = Tournament(name=name, place=place, date=date, description=description,
                                      number_of_rounds=number_of_rounds)
+        self.tournament_table.update_tournament(self.tournament)
 
     def create_first_round(self, match_list, round_id):
         first_round = Tour(round_id, "Round 1", datetime.now(), match_list)
         round_list = [first_round]
         self.tournament.rounds = round_list
+        self.round_table.update_round(first_round)
+        self.round_table.update_rounds(self.tournament.rounds)
 
     def create_next_round(self, round_number, match_list, round_id):
         next_round = Tour(round_id, "Round {}".format(round_number), datetime.now(), match_list)
         self.tournament.rounds.append(next_round)
+        self.round_table.update_round(next_round)
+        self.round_table.update_rounds(self.tournament.rounds)
         return next_round
 
     def check_if_match_already_happened(self, player_a, player_b):
