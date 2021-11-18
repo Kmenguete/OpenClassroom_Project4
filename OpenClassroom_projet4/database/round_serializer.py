3c6310ec0b73980b8ed09fab1128502176836c52
Kmenguete <menguetekevin@gmail.com>
2021-11-18 10:49:55 +0100

    Converting datetime into string in order to serialize it

--- OpenClassroom_projet4/database/round_serializer.py
+++ OpenClassroom_projet4/database/round_serializer.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from OpenClassroom_projet4.model.Round_model import Tour
 
 
@@ -5,7 +7,9 @@ class RoundSerializer:
 
     @staticmethod
     def serialize(round: Tour):
-        serialized_round = {'round_id': round.round_id, 'round_name': round.round_name, 'start_date': round.start_date,
+        now = datetime.now()
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+        serialized_round = {'round_id': round.round_id, 'round_name': round.round_name, 'start_date': date_time,
                             'matches': round.matches}
         return serialized_round
 
