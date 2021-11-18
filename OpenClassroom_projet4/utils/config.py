752c7c24555767a48b610f7c75291bae3e46cbce
Kmenguete <menguetekevin@gmail.com>
2021-11-18 10:04:30 +0100

    Creation of a database as a global variable

--- OpenClassroom_projet4/utils/config.py
+++ OpenClassroom_projet4/utils/config.py
@@ -1,3 +1,4 @@
+from tinydb import TinyDB
 
 
 class Config:
@@ -5,3 +6,4 @@ class Config:
     PLAYER_A_EXPECTED_INPUT = 'A'
     PLAYER_B_EXPECTED_INPUT = 'B'
     NO_WINNER_EXPECTED_INPUT = 'None'
+    DATABASE = TinyDB('database.json')
