5bd9b24d626ff9d8c4e3d8d1810a325f9e9184d1
Kmenguete <menguetekevin@gmail.com>
2021-11-18 13:23:51 +0100

    I am attaching players, matches and rounds together in the database.

--- OpenClassroom_projet4/database/round_table.py
+++ OpenClassroom_projet4/database/round_table.py
@@ -2,7 +2,11 @@ from datetime import datetime
 
 from tinydb import Query
 
+from OpenClassroom_projet4.database.match_serializer import MatchSerializer
+from OpenClassroom_projet4.database.player_serializer import PlayerSerializer
 from OpenClassroom_projet4.database.round_serializer import RoundSerializer
+from OpenClassroom_projet4.model.Match_model import Match
+from OpenClassroom_projet4.model.Player_model import Player
 from OpenClassroom_projet4.model.Round_model import Tour
 from OpenClassroom_projet4.utils.config import Config
 
@@ -49,10 +53,20 @@ class RoundTable:
 
 if __name__ == '__main__':
     now = datetime.now()
+    player_3 = Player(last_name='Don', firstname='Kyrus', rank=8, player_id='46464646QSDFSD64', total_score=0)
+    player_4 = Player(last_name='Al awal', firstname='Nawal', rank=1, player_id='1234RF5456GH46TFD', total_score=4)
+    player_a = PlayerSerializer().serialize(player_3)
+    player_b = PlayerSerializer().serialize(player_4)
+    match_1 = Match(match_id='Q7STS76FS87G', player_a=player_a, score_player_a=0, player_b=player_b,
+                    score_player_b=1)
+    match_2 = Match(match_id='Q7STS76FS87G', player_a=player_a, score_player_a=0.5, player_b=player_b,
+                    score_player_b=0.5)
+    serialized_match_1 = MatchSerializer().serialize(match_1)
+    serialized_match_2 = MatchSerializer().serialize(match_2)
     round_3 = Tour(round_id='EER7FT7ZF32874RG', round_name='Round_1', start_date=now,
-                   matches=['match_1', 'match_2', 'match_3', 'match_4'])
+                   matches=[serialized_match_1, serialized_match_2])
     round_4 = Tour(round_id='9854UT948T458T9', round_name='Round_2', start_date=now,
-                   matches=['match_1', 'match_2', 'match_3', 'match_4'])
+                   matches=[serialized_match_1, serialized_match_2])
     RoundTable().save_rounds([round_3, round_4])
     dsr = RoundTable().get_rounds()
     print(dsr)
