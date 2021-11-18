59587611b9532903aef39df757bcff8188b17e4d
Kmenguete <menguetekevin@gmail.com>
2021-11-18 17:08:54 +0100

    Integration of serialized players in serialized matches and serialized matches in serialized rounds.

--- OpenClassroom_projet4/database/match_serializer.py
+++ OpenClassroom_projet4/database/match_serializer.py
@@ -1,12 +1,17 @@
+from OpenClassroom_projet4.database.player_serializer import PlayerSerializer
 from OpenClassroom_projet4.model.Match_model import Match
 
 
 class MatchSerializer:
 
-    @staticmethod
-    def serialize(match: Match):
-        serialized_match = {'match_id': match.match_id, 'player_a': match.player_a,
-                            'score_player_a': match.score_player_a, 'player_b': match.player_b,
+    def __init__(self):
+        self.player_serializer = PlayerSerializer()
+
+    def serialize(self, match: Match):  
+        serialized_player_a = self.player_serializer.serialize(match.player_a)
+        serialized_player_b = self.player_serializer.serialize(match.player_b)
+        serialized_match = {'match_id': match.match_id, 'player_a': serialized_player_a,
+                            'score_player_a': match.score_player_a, 'player_b': serialized_player_b,
                             'score_player_b': match.score_player_b}
         return serialized_match
 
--- OpenClassroom_projet4/database/round_serializer.py
+++ OpenClassroom_projet4/database/round_serializer.py
@@ -1,16 +1,23 @@
 from datetime import datetime
 
+from OpenClassroom_projet4.database.match_serializer import MatchSerializer
 from OpenClassroom_projet4.model.Round_model import Tour
 
 
 class RoundSerializer:
 
-    @staticmethod
-    def serialize(round: Tour):
+    def __init__(self):
+        self.match_serializer = MatchSerializer()
+
+    def serialize(self, round: Tour):
         now = datetime.now()
         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+        serialized_matches = []
+        for match in round.matches:
+            serialized_match = self.match_serializer.serialize(match)
+            serialized_matches.append(serialized_match)
         serialized_round = {'round_id': round.round_id, 'round_name': round.round_name, 'start_date': date_time,
-                            'matches': round.matches}
+                            'matches': serialized_matches}
         return serialized_round
 
     @staticmethod
