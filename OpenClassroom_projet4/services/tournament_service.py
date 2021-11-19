2598b5ae069f901118140d53063276b271e00213
Kmenguete <menguetekevin@gmail.com>
2021-11-19 13:34:03 +0100

    Updating tournament_service

--- OpenClassroom_projet4/services/tournament_service.py
+++ OpenClassroom_projet4/services/tournament_service.py
@@ -1,6 +1,4 @@
 from datetime import datetime
-
-from OpenClassroom_projet4.database.round_table import RoundTable
 from OpenClassroom_projet4.database.tournament_table import TournamentTable
 from OpenClassroom_projet4.model.Round_model import Tour
 from OpenClassroom_projet4.model.Tournament_model import Tournament
@@ -9,27 +7,34 @@ from OpenClassroom_projet4.model.Tournament_model import Tournament
 class TournamentService:
 
     def __init__(self):
-        self.tournament = None
         self.tournament_table = TournamentTable()
-        self.round_table = RoundTable()
+        self.tournament = None
+
+    def update_players(self, players):
+        self.tournament_table.update_players(tournament_id=self.tournament.tournament_id, players=players)
+        self.tournament = self.tournament_table.get_tournament(tournament_id=self.tournament.tournament_id)
 
     def update_tournament(self, name, place, date, description, number_of_rounds):
-        self.tournament = Tournament(name=name, place=place, date=date, description=description,
-                                     number_of_rounds=number_of_rounds)
-        self.tournament_table.save_tournament(self.tournament)
+        tournament = Tournament(name=name, place=place, date=date, description=description,
+                                number_of_rounds=number_of_rounds)
+        self.tournament_table.save_tournament(tournament)
+        self.tournament = self.tournament_table.get_tournament(tournament_id=tournament.tournament_id)
+
+    def update_round_matches(self, round, match_list):
+        self.tournament_table.update_round_matches(self.tournament.tournament_id, round, match_list)
 
     def create_first_round(self, match_list, round_id):
         first_round = Tour(round_id, "Round 1", datetime.now(), match_list)
         round_list = [first_round]
-        self.tournament.rounds = round_list
-        self.round_table.save_round(first_round)
-        self.round_table.save_rounds(self.tournament.rounds)
+        self.tournament_table.update_rounds(tournament_id=self.tournament.tournament_id, new_rounds=round_list)
+        self.tournament = self.tournament_table.get_tournament(tournament_id=self.tournament.tournament_id)
 
     def create_next_round(self, round_number, match_list, round_id):
         next_round = Tour(round_id, "Round {}".format(round_number), datetime.now(), match_list)
-        self.tournament.rounds.append(next_round)
-        self.round_table.save_round(next_round)
-        self.round_table.save_rounds(self.tournament.rounds)
+        rounds = self.tournament_table.get_rounds(tournament_id=self.tournament.tournament_id)
+        rounds.append(next_round)
+        self.tournament_table.update_rounds(tournament_id=self.tournament.tournament_id, new_rounds=rounds)
+        self.tournament = self.tournament_table.get_tournament(tournament_id=self.tournament.tournament_id)
         return next_round
 
     def check_if_match_already_happened(self, player_a, player_b):
