4fab6017da6078c978efa4465f71ff26c52ca3ea
Kmenguete <menguetekevin@gmail.com>
2021-11-19 13:41:30 +0100

    Indenting tournament_id to the right place

--- OpenClassroom_projet4/model/Tournament_model.py
+++ OpenClassroom_projet4/model/Tournament_model.py
@@ -27,7 +27,7 @@ class Tournament:
     def get_tournament_id(tournament_id):
         if tournament_id is None:
             tournament_id = str(uuid.uuid4())
-            return tournament_id
+        return tournament_id
 
     def __str__(self):
         return "name: " + self.name + ", " + "place: " + self.place + ", " + "description: " + self.description + ", " \
