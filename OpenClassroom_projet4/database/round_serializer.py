6fb5fe3383ebd829d34c9ed9dea62ebfb4c24578
Kmenguete <menguetekevin@gmail.com>
2021-11-19 13:12:30 +0100

    Updating round serializer

--- OpenClassroom_projet4/database/round_serializer.py
+++ OpenClassroom_projet4/database/round_serializer.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from OpenClassroom_projet4.database.match_serializer import MatchSerializer
 from OpenClassroom_projet4.model.Round_model import Tour
+from OpenClassroom_projet4.utils.config import Config
 
 
 class RoundSerializer:
@@ -10,17 +11,23 @@ class RoundSerializer:
         self.match_serializer = MatchSerializer()
 
     def serialize(self, round: Tour):
-        now = datetime.now()
-        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
         serialized_matches = []
         for match in round.matches:
             serialized_match = self.match_serializer.serialize(match)
             serialized_matches.append(serialized_match)
+
+        date_time = round.start_date.strftime(Config.DATE_STRING_FORMAT)
         serialized_round = {'round_id': round.round_id, 'round_name': round.round_name, 'start_date': date_time,
                             'matches': serialized_matches}
         return serialized_round
 
-    @staticmethod
-    def deserialize(serialized_round: dict):
+    def deserialize(self, serialized_round: dict):
+        deserialized_matches = []
+        for serialized_match in serialized_round['matches']:
+            match = self.match_serializer.deserialize(serialized_match)
+            deserialized_matches.append(match)
+
+        date_time_obj = datetime.strptime(serialized_round['start_date'], Config.DATE_STRING_FORMAT)
+
         return Tour(round_id=serialized_round['round_id'], round_name=serialized_round['round_name'],
-                    start_date=serialized_round['start_date'], matches=serialized_round['matches'])
+                    start_date=date_time_obj, matches=deserialized_matches)
