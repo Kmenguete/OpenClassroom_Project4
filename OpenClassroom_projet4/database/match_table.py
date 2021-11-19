9f32536da483348f52ea8ed2eb24b45966225da6
Kmenguete <menguetekevin@gmail.com>
2021-11-19 13:25:13 +0100

    Updating match table

--- OpenClassroom_projet4/database/match_table.py
+++ OpenClassroom_projet4/database/match_table.py
@@ -1,7 +1,5 @@
-from tinydb import Query
-
+from tinydb import Query, TinyDB
 from OpenClassroom_projet4.database.match_serializer import MatchSerializer
-from OpenClassroom_projet4.database.player_serializer import PlayerSerializer
 from OpenClassroom_projet4.model.Match_model import Match
 from OpenClassroom_projet4.model.Player_model import Player
 from OpenClassroom_projet4.utils.config import Config
@@ -10,17 +8,17 @@ from OpenClassroom_projet4.utils.config import Config
 class MatchTable:
 
     def __init__(self):
-        self.database = Config.DATABASE
+        self.database = TinyDB('db.json')
+        self.match_database = self.database.table(Config.MATCH_TABLE_NAME)
         self.match_serializer = MatchSerializer()
-        self.match_table = self.database.table('Match_table')
 
     def save_match(self, match):
         serialized_match = self.match_serializer.serialize(match)
-        self.match_table.insert(serialized_match)
+        self.match_database.insert(serialized_match)
 
     def get_match(self, match_id):
         match_query = Query()
-        serialized_match = self.match_table.search(match_query.match_id == match_id)
+        serialized_match = self.match_database.search(match_query.match_id == match_id)
         match = self.match_serializer.deserialize(serialized_match[0])
         return match
 
@@ -29,35 +27,31 @@ class MatchTable:
         for match in matches:
             serialized_match = self.match_serializer.serialize(match)
             serialized_matches.append(serialized_match)
-        self.match_table.insert_multiple(serialized_matches)
+        self.match_database.insert_multiple(serialized_matches)
 
     def get_matches(self):
-        serialized_matches = self.match_table.all()
+        serialized_matches = self.match_database.all()
         matches = []
         for serialized_match in serialized_matches:
             match = self.match_serializer.deserialize(serialized_match)
             matches.append(match)
         return matches
 
-    def update_matches(self, matches):
-        self.match_table.update_multiple(matches)
-
     def update_match(self, match_id):
         match = Query()
-        self.match_table.update({'match_id': match_id}, match.match_id == match_id)
+        self.match_database.update({'match_id': match_id}, match.match_id == match_id)
 
-    def clear_match_table(self):
-        self.match_table.truncate()
+    def clear_database(self):
+        self.match_database.truncate()
 
 
 if __name__ == '__main__':
-    player_3 = Player(last_name='Don', firstname='Kyrus', rank=8, player_id='46464646QSDFSD64', total_score=0)
-    player_4 = Player(last_name='Al awal', firstname='Nawal', rank=1, player_id='1234RF5456GH46TFD', total_score=4)
-    player_a = PlayerSerializer().serialize(player_3)
-    player_b = PlayerSerializer().serialize(player_4)
-    match_1 = Match(match_id='Q7STS76FS87G', player_a=player_a, score_player_a=0, player_b=player_b,
+    player_1 = Player(last_name='Darden', firstname='Mike', rank=5, player_id='46464646QSDFSD64', total_score=3)
+    player_2 = Player(last_name='Mikaela', firstname='Arnold', rank=7, player_id='1234RF5456GH46TFD', total_score=1)
+
+    match_1 = Match(match_id='Q7STS76FS87G', player_a=player_1, score_player_a=0, player_b=player_2,
                     score_player_b=1)
-    match_2 = Match(match_id='Q7STS76FS87G', player_a=player_a, score_player_a=0.5, player_b=player_b,
+    match_2 = Match(match_id='Q7STS76FS87G', player_a=player_1, score_player_a=0.5, player_b=player_2,
                     score_player_b=0.5)
     MatchTable().save_matches([match_1, match_2])
     dsm = MatchTable().get_matches()
