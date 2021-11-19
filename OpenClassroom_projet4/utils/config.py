6604ab0898b16d8d75bf2ee865b6614bf5309730
Kmenguete <menguetekevin@gmail.com>
2021-11-19 13:07:35 +0100

    Updating config file

--- OpenClassroom_projet4/utils/config.py
+++ OpenClassroom_projet4/utils/config.py
@@ -1,9 +1,13 @@
-from tinydb import TinyDB
-
 
 class Config:
     DEFAULT_PLAYERS_NUMBER = 8
     PLAYER_A_EXPECTED_INPUT = 'A'
     PLAYER_B_EXPECTED_INPUT = 'B'
     NO_WINNER_EXPECTED_INPUT = 'None'
-    DATABASE = TinyDB('database.json')
+
+    PLAYER_TABLE_NAME = "players"
+    MATCH_TABLE_NAME = "matches"
+    ROUND_TABLE_NAME = "rounds"
+    TOURNAMENT_TABLE_NAME = "tournaments"
+
+    DATE_STRING_FORMAT = "%m/%d/%Y, %H:%M:%S"
