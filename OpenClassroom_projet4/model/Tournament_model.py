86946b7965e1127c175ea8f8908d4556be13c296
Kmenguete <menguetekevin@gmail.com>
2021-11-25 11:09:27 +0100

    Removing unused method from Tournament_model

--- OpenClassroom_projet4/model/Tournament_model.py
+++ OpenClassroom_projet4/model/Tournament_model.py
@@ -42,34 +42,3 @@ class Tournament:
 
     def __getitem__(self, players, i):
         return self.players[i]
-
-    def check_if_match_already_happened(self, player_a, player_b):
-        already_happened = False
-        for tour in self.rounds:
-            for match in tour.matches:
-                if (match.player_a == player_a and match.player_b == player_b) or (match.player_a == player_b and
-                                                                                   match.player_b == player_a):
-                    already_happened = True
-        return already_happened
-
-    def get_next_available_player(self, player_a, next_player_index, non_available_players):
-        index = next_player_index
-        while self.check_if_match_already_happened(player_a, self.players[index]) or \
-                self.players[index] in non_available_players:
-            index += 1
-        player_b = self.players[index]
-        return player_b
-
-    def seek_player_and_update_score(self, player, new_score):
-        if player.player_id in self.players_dict:
-            self.players_dict[player.player_id] += new_score
-        else:
-            self.players_dict[player.player_id] = player.total_score + new_score
-        return self.players_dict
-
-    def display_players(self):
-        print("******************** list of players with length: {} *******************".format(len(self.players_dict)))
-        print("Here our players are sorted by their score. On the left, this is the player ID. Each player ID is "
-              "unique. On the right this is the total score of each player.")
-        for player in self.players_dict.items():
-            print(player)
