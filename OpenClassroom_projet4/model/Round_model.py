03882a190556a91ec3c207042c40bab3c6112c1c
Kmenguete <menguetekevin@gmail.com>
2021-11-25 15:05:32 +0100

    comment with docstring round model

--- OpenClassroom_projet4/model/Round_model.py
+++ OpenClassroom_projet4/model/Round_model.py
@@ -4,6 +4,11 @@ from datetime import datetime
 
 @dataclass
 class Tour:
+    """ The round model is the class that let the application to create a round object(and their corresponding matches
+    object). When the application creates a round, it automatically creates matches object (from match model) with
+    players (from player model). Of course, players object are players that have been created by the user when we asked
+    him/her to create players.
+    """
     round_id: str
     round_name: str
     start_date: datetime
