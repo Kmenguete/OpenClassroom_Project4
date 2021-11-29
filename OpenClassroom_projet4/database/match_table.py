e1c62c2bec0837b10d29f29ead13dde9b0b87576
Kmenguete <menguetekevin@gmail.com>
2021-11-29 16:39:34 +0100

    Updating match table documentation

--- OpenClassroom_projet4/database/match_table.py
+++ OpenClassroom_projet4/database/match_table.py
@@ -1,7 +1,5 @@
 from tinydb import Query, TinyDB
 from OpenClassroom_projet4.serializers.match_serializer import MatchSerializer
-from OpenClassroom_projet4.model.match_model import Match
-from OpenClassroom_projet4.model.player_model import Player
 from OpenClassroom_projet4.utils.config import Config
 
 
@@ -11,21 +9,29 @@ class MatchTable:
     """
 
     def __init__(self):
+        """ The init method is used to import every modules required to save and update matches.
+            """
         self.database = TinyDB(Config.DATABASE_NAME)
         self.match_database = self.database.table(Config.MATCH_TABLE_NAME)
         self.match_serializer = MatchSerializer()
 
     def save_match(self, match):
+        """ The save_match method is used to save one match.
+                    """
         serialized_match = self.match_serializer.serialize(match)
         self.match_database.insert(serialized_match)
 
     def get_match(self, match_id):
+        """ The get_match method is used to retrieve one match from the database.
+                            """
         match_query = Query()
         serialized_match = self.match_database.search(match_query.match_id == match_id)
         match = self.match_serializer.deserialize(serialized_match[0])
         return match
 
     def save_matches(self, matches: list):
+        """ The save_matches method is used to save a list of matches.
+                            """
         serialized_matches = []
         for match in matches:
             serialized_match = self.match_serializer.serialize(match)
@@ -33,31 +39,22 @@ class MatchTable:
         self.match_database.insert_multiple(serialized_matches)
 
     def get_matches(self):
+        """ The get_matches method is used to retrieve a list of matches from the database.
+                                    """
         serialized_matches = self.match_database.all()
         matches = []
         for serialized_match in serialized_matches:
             match = self.match_serializer.deserialize(serialized_match)
             matches.append(match)
-        for match in matches:
-            print(match)
         return matches
 
     def update_match(self, match_id):
+        """ The update_match method is used to update one match in the database.
+                                            """
         match = Query()
         self.match_database.update({'match_id': match_id}, match.match_id == match_id)
 
     def clear_database(self):
+        """ The clear_database method is used to delete every match object in the database.
+                                                    """
         self.match_database.truncate()
-
-
-if __name__ == '__main__':
-    player_1 = Player(last_name='Darden', firstname='Mike', rank=5, player_id='46464646QSDFSD64', total_score=3)
-    player_2 = Player(last_name='Mikaela', firstname='Arnold', rank=7, player_id='1234RF5456GH46TFD', total_score=1)
-
-    match_1 = Match(match_id='Q7STS76FS87G', player_a=player_1, score_player_a=0, player_b=player_2,
-                    score_player_b=1)
-    match_2 = Match(match_id='Q7STS76FS87G', player_a=player_1, score_player_a=0.5, player_b=player_2,
-                    score_player_b=0.5)
-    MatchTable().save_matches([match_1, match_2])
-    dsm = MatchTable().get_matches()
-    print(dsm)
