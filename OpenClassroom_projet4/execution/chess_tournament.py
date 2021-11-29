d26eb1783c61ec0cb696da390de003d61d35ba17
Kmenguete <menguetekevin@gmail.com>
2021-11-29 10:22:49 +0100

    updating chess tournament dependency and renaming Execution directory ===> execution

--- OpenClassroom_projet4/execution/chess_tournament.py
+++ OpenClassroom_projet4/execution/chess_tournament.py
@@ -1,6 +1,6 @@
 """ The main file is used to run the application. Without this file, it is not possible to run the application.
 """
-from OpenClassroom_projet4.controller.controller import MainController
+from OpenClassroom_projet4.controller.main_controller import MainController
 
 
 if __name__ == '__main__':
