87e8d62d292149ff765755f2b50561e42808429e
Kmenguete <menguetekevin@gmail.com>
2021-11-29 11:08:25 +0100

    deleting unused methods

--- OpenClassroom_projet4/services/report_service.py
+++ OpenClassroom_projet4/services/report_service.py
@@ -4,7 +4,6 @@ from OpenClassroom_projet4.database.player_table import PlayerTable
 from OpenClassroom_projet4.database.round_table import RoundTable
 from OpenClassroom_projet4.database.tournament_table import TournamentTable
 from OpenClassroom_projet4.errors.exceptions import TournamentDoesNotExistError
-from OpenClassroom_projet4.model.tournament_model import DEFAULT_ROUNDS_NUMBER
 
 
 class ReportService:
@@ -68,58 +67,3 @@ class ReportService:
     @staticmethod
     def get_round_matches(round):
         return round.matches
-
-    def way_of_display_player_list(self):
-        self.select_one_tournament()
-        way_of_display_players = input('If you want players sorted by rank. type A: \n '
-                                       'If you want players sorted alphabetically. type B: ')
-        if way_of_display_players == 'A':
-            self.get_sorted_player_list_by_rank()
-        elif way_of_display_players == 'B':
-            self.get_sorted_player_list_alphabetically()
-        else:
-            print('Invalid value, you should answer the question by A or B.')
-
-    def get_rounds_of_one_tournament(self):
-        tournament = self.select_one_tournament()
-        print(tournament.rounds)
-
-    def get_matches_of_one_tournament(self):
-        tournament = self.select_one_tournament()
-        for round_index in range(0, DEFAULT_ROUNDS_NUMBER):
-            print(tournament.rounds[round_index].matches)
-
-    def select_one_tournament(self):
-        self.tournament_table.get_tournaments()
-        tournament = self.tournament_table.get_tournament(tournament_id=input('Please select one '
-                                                                              'tournament by their id: '))
-        tournament_selection = tournament
-        if tournament_selection != tournament:
-            print('The tournament you selected does not exist. Please, select a tournament that exist.')
-        else:
-            self.tournament_table.get_tournament(tournament)
-        return tournament
-
-    def suggest_report(self):
-        report_suggestion = input('Do you want a report? Yes/No: ')
-        if report_suggestion != 'Yes' and report_suggestion != 'No':
-            print('Invalid value, you should answer the question by Yes or No.')
-        else:
-            if report_suggestion == 'No':
-                print('Thank you for your answer. Good bye !!!')
-                exit()
-            else:
-                multiple_choice_question = input('If you want the list of tournaments, type A: \n '
-                                                 'If you want the list of players of one tournament, type B: \n '
-                                                 'If you want the list of rounds of one tournament, type C: \n '
-                                                 'If you want the list of matches of one tournament, type D:')
-                if multiple_choice_question == 'A':
-                    self.tournament_table.get_tournaments()
-                elif multiple_choice_question == 'B':
-                    self.way_of_display_player_list()
-                elif multiple_choice_question == 'C':
-                    self.get_rounds_of_one_tournament()
-                elif multiple_choice_question == 'D':
-                    self.get_matches_of_one_tournament()
-                else:
-                    print('Invalid value, you should answer the question by either A,B,C or D.')
