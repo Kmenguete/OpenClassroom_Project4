f548131ea2d3f7f4ab929bf92b25232ff237da80
Kmenguete <menguetekevin@gmail.com>
2021-11-29 13:59:45 +0100

    I am splitting the request report method in smaller methods

--- OpenClassroom_projet4/controller/reports_controller.py
+++ OpenClassroom_projet4/controller/reports_controller.py
@@ -23,25 +23,10 @@ class ReportsController:
             self.manage_report_choice_4()
         elif choice == '5':
             self.manage_report_choice_5()
-            self.another_report_suggestion()
         elif choice == '6':
-            self.tournament_table.display_tournaments()
-            tournament_id = View.get_tournament_id()
-            try:
-                rounds = self.report_service.get_tournament_rounds(tournament_id)
-                View.display_rounds(rounds)
-                self.another_report_suggestion()
-            except TournamentDoesNotExistError:
-                View.display_invalid_tournament_id_message(tournament_id)
+            self.manage_report_choice_6()
         elif choice == '7':
-            self.tournament_table.display_tournaments()
-            tournament_id = View.get_tournament_id()
-            try:
-                games = self.report_service.get_all_tournament_matches(tournament_id)
-                View.display_all_games(games)
-                self.another_report_suggestion()
-            except TournamentDoesNotExistError:
-                View.display_invalid_tournament_id_message(tournament_id)
+            self.manage_report_choice_7()
         else:
             View.display_text('Invalid value, enter one of the menu numbers')
 
@@ -92,6 +77,28 @@ class ReportsController:
             try:
                 players = self.report_service.get_tournament_players_sorted_by_rank(tournament_id)
                 View.display_players(players)
-                break
+                self.another_report_suggestion()
+            except TournamentDoesNotExistError:
+                View.display_invalid_tournament_id_message(tournament_id)
+
+    def manage_report_choice_6(self):
+        while True:
+            self.tournament_table.display_tournaments()
+            tournament_id = View.get_tournament_id()
+            try:
+                rounds = self.report_service.get_tournament_rounds(tournament_id)
+                View.display_rounds(rounds)
+                self.another_report_suggestion()
+            except TournamentDoesNotExistError:
+                View.display_invalid_tournament_id_message(tournament_id)
+
+    def manage_report_choice_7(self):
+        while True:
+            self.tournament_table.display_tournaments()
+            tournament_id = View.get_tournament_id()
+            try:
+                games = self.report_service.get_all_tournament_matches(tournament_id)
+                View.display_all_games(games)
+                self.another_report_suggestion()
             except TournamentDoesNotExistError:
                 View.display_invalid_tournament_id_message(tournament_id)
