92745e78523bae34348b40473514a17e92ff7aef
Kmenguete <menguetekevin@gmail.com>
2021-11-29 15:12:10 +0100

    updating game_controller.py

--- OpenClassroom_projet4/controller/game_controller.py
+++ OpenClassroom_projet4/controller/game_controller.py
@@ -12,26 +12,13 @@ from OpenClassroom_projet4.view.view import View
 
 
 class GameController:
-    """ The MainController class ensures a proper running of the tournament. To work, the MainController class uses
-    different modules and packages. First of all, we ask the user to create a tournament. Once the tournament is
-    created, we ask him/her to create players. When players are created, the application create automatically a first
-    round. In our application, a round consist of a math list, a date, a round name and a round id. The first round
-    is voluntarily separated from other rounds because the logic to which matches are generated in the firs round is
-    very different from the logic to which matches are generated on other rounds. Once a first round is created with
-    his matches, we then, ask the user to enter the result of each match. The result of the match is known only when
-    a match(chess match) is finished. Once, the user entered results, every players are sorted by their total score.
-    Next, the second round is generated and we ask the user to enter the result of matches. And then, we repeat the
-    process until the end of the tournament. For each round, the user enter the results of matches, players are
-    sorted by their total scores and the next round start. When the tournament is finished, the rank is updating
-    according the total score of each players. When two players has the same total score then we promote the former
-    rank of both players. And finally, when a tournament is finished, it is saved in a database named "db.json". At
-    the end of the tournament, we suggest a report to the user. If the user does not want a report then the
-    application stop running otherwise the user choose which information he/she wants from the database(list of
-    tournaments, list of players of a tournament... .etc.). When the user have chosen which information he/she wants
-    from the report, the application displays the information requested by the user and then stop running.
+    """ The Game controller aim to make sure that the tournament runs properly.
     """
 
     def __init__(self):
+        """ The init method import every required modules to save and update the tournament during its progress.
+            """
+
         self.player_service = PlayerService()
         self.match_service = MatchService()
         self.tournament_service = TournamentService()
@@ -39,6 +26,8 @@ class GameController:
         self.round_id = str(uuid.uuid4())
 
     def run_game(self):
+        """ The run game method call every methods that let to run, save and update the tournament.
+                    """
         self.create_tournament()
         self.tournament_service.save()
 
@@ -66,6 +55,8 @@ class GameController:
         View.display_players(self.player_service.players_dict)
 
     def create_tournament(self):
+        """ The create tournament method let the user to create a tournament.
+                            """
         name_choice, place_choice, date_choice, description_choice = View.get_tournament_information()
         self.tournament_service.update_tournament(name=name_choice, place=place_choice, date=date_choice,
                                                   description=description_choice,
@@ -73,6 +64,8 @@ class GameController:
         # print("Tournament === " + str(self.tournament_service.tournament))
 
     def create_players(self):
+        """ The create players method let the user to create players.
+                                    """
         player_list = []
         for index in range(Config.DEFAULT_PLAYERS_NUMBER):
             first_name, last_name = View.get_player_information()
@@ -87,10 +80,15 @@ class GameController:
         # self.player_service.update_player_list(player_list)
 
     def render_all_matches(self):
+        """ The render all matches method is called every where we need to display matches in order to know which
+        players has to meet each other in a given round.
+                                            """
         for match in self.match_service.match_list:
             View.display_match_information(match)
 
     def generate_first_round(self):
+        """ Once, the user have registered the 8 players, the application generate the first round with its matches.
+                                                    """
         # Step 3: Generate first round players pair
         self.player_service.player_list = self.tournament_service.tournament.players
         player_pairs = self.player_service.generate_initial_player_pair()
@@ -111,6 +109,11 @@ class GameController:
         print("Tournament === " + str(self.tournament_service.tournament))
 
     def get_initial_round_results_and_update(self, round_name="Round 1"):
+        """ When a match is finished, the user enter the result. the user repeat the process for every matches
+        of the round 1. We voluntarily separated the round 1 from other rounds because the logic to which matches
+        are generated in the first round is very different from the logic to which matches are generated on the
+        other rounds.
+                                                    """
         View.display_text("\n *************** Enter the results for {} **************".format(round_name))
         self.get_round_results(self.tournament_service.tournament.rounds[0])
         # self.tournament_service.update_round_matches(self.tournament_service.tournament.rounds[0], updated_match_list)
