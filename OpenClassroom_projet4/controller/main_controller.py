776e37eb6dbef95a44873f34f80765a0f578ef3f
Kmenguete <menguetekevin@gmail.com>
2021-11-29 15:57:45 +0100

    updating main controller documentation

--- OpenClassroom_projet4/controller/main_controller.py
+++ OpenClassroom_projet4/controller/main_controller.py
@@ -4,12 +4,21 @@ from OpenClassroom_projet4.view.view import View
 
 
 class MainController:
+    """ The Main controller ask the user whether he/she wants to play a tournament or request a report.
+            """
 
     def __init__(self):
+        """ The init method import the game controller and the reports controller. The game controller is used to play
+        a tournament. The reports controller is used to request a report.
+                    """
         self.game_controller = GameController()
         self.reports_controller = ReportsController()
 
     def start(self):
+        """ The start method is the method that is called in the execution file of the application. When the application
+        is executed, we ask the user whether he/she wants to play a tournament or to request a report. If the user
+        choose 1, then a new tournament start. If he/she choose 2, the application will display the report menu.
+                            """
         View.display_initial_menu()
         choice = View.get_choice("Choice: ")
         if choice == '1':
