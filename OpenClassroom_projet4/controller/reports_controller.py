93110d757e2cf1aa8fe11450bf6826e836b72ef6
Kmenguete <menguetekevin@gmail.com>
2021-11-29 13:32:56 +0100

    Adding the possibility to come back to report menu once a user got the information requested from the report service

--- OpenClassroom_projet4/controller/reports_controller.py
+++ OpenClassroom_projet4/controller/reports_controller.py
@@ -16,28 +16,34 @@ class ReportsController:
         if choice == '1':
             players = self.report_service.get_sorted_player_list_alphabetically()
             View.display_players(players)
+            self.another_report_suggestion()
         elif choice == '2':
             players = self.report_service.get_sorted_player_list_by_rank()
             View.display_players(players)
+            self.another_report_suggestion()
         elif choice == '3':
             tournaments = self.report_service.get_all_tournaments()
             View.display_tournaments(tournaments)
+            self.another_report_suggestion()
         elif choice == '4':
             self.tournament_table.display_tournaments()
             tournament_id = View.get_tournament_id()
             try:
                 players = self.report_service.get_tournament_players_in_alphabetical_order(tournament_id)
                 View.display_players(players)
+                self.another_report_suggestion()
             except TournamentDoesNotExistError:
                 View.display_invalid_tournament_id_message(tournament_id)
         elif choice == '5':
             self.manage_report_choice_5()
+            self.another_report_suggestion()
         elif choice == '6':
             self.tournament_table.display_tournaments()
             tournament_id = View.get_tournament_id()
             try:
                 rounds = self.report_service.get_tournament_rounds(tournament_id)
                 View.display_rounds(rounds)
+                self.another_report_suggestion()
             except TournamentDoesNotExistError:
                 View.display_invalid_tournament_id_message(tournament_id)
         elif choice == '7':
@@ -46,10 +52,21 @@ class ReportsController:
             try:
                 games = self.report_service.get_all_tournament_matches(tournament_id)
                 View.display_all_games(games)
+                self.another_report_suggestion()
             except TournamentDoesNotExistError:
                 View.display_invalid_tournament_id_message(tournament_id)
         else:
             View.display_text('Invalid value, enter one of the menu numbers')
+
+    def another_report_suggestion(self):
+        choice = View.get_choice("Do you want another report? Yes/No: ")
+        if choice == 'Yes':
+            self.request_report()
+        elif choice == 'No':
+            View.display_text("Thank you for your answer !!! Good bye !!!")
+            exit()
+        else:
+            View.display_text("Invalid answer. You should answer the question by Yes or No")
 
     def manage_report_choice_5(self):
         while True:
