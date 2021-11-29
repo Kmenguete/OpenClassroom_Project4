2ded677baaa45297435725e85d123c1c3a0a2c19
Kmenguete <menguetekevin@gmail.com>
2021-11-29 09:53:11 +0100

    Updating report service

--- OpenClassroom_projet4/services/report_service.py
+++ OpenClassroom_projet4/services/report_service.py
@@ -3,8 +3,8 @@ from OpenClassroom_projet4.database.match_table import MatchTable
 from OpenClassroom_projet4.database.player_table import PlayerTable
 from OpenClassroom_projet4.database.round_table import RoundTable
 from OpenClassroom_projet4.database.tournament_table import TournamentTable
+from OpenClassroom_projet4.errors.exceptions import TournamentDoesNotExistError
 from OpenClassroom_projet4.model.Tournament_model import DEFAULT_ROUNDS_NUMBER
-from OpenClassroom_projet4.utils.config import Config
 
 
 class ReportService:
@@ -19,28 +19,64 @@ class ReportService:
         self.round_table = RoundTable()
         self.tournament_table = TournamentTable()
 
-    @staticmethod
-    def get_sorted_player_list_alphabetically(player_list):
-        sorted_player_list = [player_list[player_index].last_name + " " + player_list[player_index].firstname
-                              for player_index in range(0, Config.DEFAULT_PLAYERS_NUMBER)]
-        sorted_player_list_alphabetically = sorted(sorted_player_list)
-        print(sorted_player_list_alphabetically)
-        return sorted_player_list_alphabetically
+    def get_tournament(self, tournament_id):
+        tournament = self.tournament_table.get_tournament(tournament_id)
+        if tournament is None:
+            raise TournamentDoesNotExistError("This tournament id does not exist")
+        return tournament
+
+    def get_sorted_player_list_alphabetically(self):
+        players = self.player_table.get_players()
+        return self.__sort_alphabetically(players)
+
+    def get_sorted_player_list_by_rank(self):
+        players = self.player_table.get_players()
+        return sorted(players, key=operator.attrgetter("rank"))
 
     @staticmethod
-    def get_sorted_player_list_by_rank(player_list):
-        sorted_player_list = sorted(player_list, key=operator.attrgetter("rank"))
-        print(sorted_player_list)
-        return sorted_player_list
+    def __sort_alphabetically(players):
+        sorted_player_list = [players[player_index].last_name + " " + players[player_index].firstname
+                              for player_index in range(0, len(players))]
+        return sorted(sorted_player_list)
+
+    @staticmethod
+    def __sort_by_rank(players):
+        return sorted(players, key=operator.attrgetter("rank"))
+
+    def get_tournament_players_in_alphabetical_order(self, tournament_id):
+        tournament = self.get_tournament(tournament_id)
+        return self.__sort_alphabetically(tournament.players)
+
+    def get_tournament_players_sorted_by_rank(self, tournament_id):
+        tournament = self.get_tournament(tournament_id)
+        return self.__sort_by_rank(tournament.players)
+
+    def get_all_tournaments(self):
+        return self.tournament_table.get_tournaments()
+
+    def get_tournament_rounds(self, tournament_id):
+        tournament = self.get_tournament(tournament_id)
+        return tournament.rounds
+
+    def get_all_tournament_matches(self, tournament_id):
+        tournament = self.get_tournament(tournament_id)
+        match_list = []
+        for round in tournament.rounds:
+            match_list.extend(round.matches)
+        return match_list
+
+    @staticmethod
+    def get_round_matches(round):
+        return round.matches
 
     def way_of_display_player_list(self):
-        tournament = self.select_one_tournament()
+        self.select_one_tournament()
         way_of_display_players = input('If you want players sorted by rank. type A: \n '
                                        'If you want players sorted alphabetically. type B: ')
         if way_of_display_players == 'A':
-            self.get_sorted_player_list_by_rank(tournament.players)
+            self.get_sorted_player_list_by_rank()
         elif way_of_display_players == 'B':
-            self.get_sorted_player_list_alphabetically(tournament.players)
+            self.get_sorted_player_list_alphabetically()
         else:
             print('Invalid value, you should answer the question by A or B.')
 
