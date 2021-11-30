023627452371e1180d2aff07ad1b7c3fdbc15a11
Kmenguete <menguetekevin@gmail.com>
2021-11-30 10:11:21 +0100

    Updating exceptions docstrings

--- OpenClassroom_projet4/errors/exceptions.py
+++ OpenClassroom_projet4/errors/exceptions.py
@@ -1,3 +1,7 @@
 
 class TournamentDoesNotExistError(Exception):
+    """ The TournamentDoesNotExistError class is called when the user ask for a tournament that does not exist in
+    the database.
+    """
+
     pass
