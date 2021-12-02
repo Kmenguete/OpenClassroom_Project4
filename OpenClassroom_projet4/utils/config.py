22ddab75a7c8ccf37b26ebdf8d0f084a315fe82a
Kmenguete <menguetekevin@gmail.com>
2021-12-02 11:49:00 +0100

    generating a flake8 report with no errors in utils directory

--- OpenClassroom_projet4/utils/config.py
+++ OpenClassroom_projet4/utils/config.py
@@ -1,13 +1,12 @@
-
 class Config:
-    """ The Config object store every constant variables of the application, except the DEFAULT_ROUNDS_NUMBER which is
+    """The Config object store every constant variables of the application, except the DEFAULT_ROUNDS_NUMBER which is
     an attribute of the tournament object.
     """
 
     DEFAULT_PLAYERS_NUMBER = 8
-    PLAYER_A_EXPECTED_INPUT = 'A'
-    PLAYER_B_EXPECTED_INPUT = 'B'
-    NO_WINNER_EXPECTED_INPUT = 'None'
+    PLAYER_A_EXPECTED_INPUT = "A"
+    PLAYER_B_EXPECTED_INPUT = "B"
+    NO_WINNER_EXPECTED_INPUT = "None"
 
     PLAYER_TABLE_NAME = "players"
     MATCH_TABLE_NAME = "matches"
