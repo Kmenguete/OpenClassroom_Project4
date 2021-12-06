4bbd427b741e39cd24802cb03f4a08975745788a
Kmenguete <menguetekevin@gmail.com>
2021-12-06 13:49:44 +0100

    refactoring execution directory

--- execution/chess_tournament.py
+++ execution/chess_tournament.py
@@ -1,6 +1,6 @@
 """ The main file is used to run the application. Without this file, it is not possible to run one application.
 """
-from OpenClassroom_projet4.controller.main_controller import MainController
+from controller.main_controller import MainController
 
 if __name__ == "__main__":
     MainController().start()
