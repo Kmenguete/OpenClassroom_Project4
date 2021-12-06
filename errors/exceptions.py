9d243fa8e1349c936d93e53bc17af4d905d824d0
Kmenguete <menguetekevin@gmail.com>
2021-12-06 13:48:46 +0100

    refactoring errors directory

--- errors/exceptions.py
+++ errors/exceptions.py
@@ -2,5 +2,4 @@ class TournamentDoesNotExistError(Exception):
     """The TournamentDoesNotExistError class is called when the user ask for a tournament that does not exist in
     the database.
     """
-
     pass
